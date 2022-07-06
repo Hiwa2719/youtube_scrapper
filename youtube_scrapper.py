4e3bcdf804d010ab2517b9577a1d94a3a1a10a87
hiwa2719 <hiahmadyan@gmail.com>
2022-07-06 20:45:27 +0430

    removing beautifulsoup from app

--- youtube_scrapper.py
+++ youtube_scrapper.py
@@ -1,11 +1,9 @@
 import asyncio
 import re
-import time
 
 import aiohttp
-from bs4 import BeautifulSoup
 from selenium import webdriver
-
+from selenium.webdriver.common.by import By
 
 message = """
 Please enter your search key word here
@@ -28,17 +26,17 @@ retrieve_number = 120  # number of channels to extract data from youtube
 search_items = {}
 
 while True:
-    soup = BeautifulSoup(browser.page_source, 'lxml')
-    content = soup.find(id='contents')
-    items = content.find_all('ytd-video-renderer', {'class': 'style-scope ytd-item-section-renderer'})
+    items = browser.find_elements(By.CSS_SELECTOR, '#contents ytd-video-renderer.style-scope.ytd-item-section-renderer')
     if len(items) > retrieve_number:
         for item in items:
-            a = item.find(id='text-container').find('a')
+            a = item.find_element(By.ID, 'text-container').find_element(By.TAG_NAME, 'a')
             search_items.update({
-                a.get('href'): a.text
+                a.get_attribute('href'): a.get_attribute('innerHTML')
             })
         break
     browser.execute_script("window.scrollBy(0, 15000)")
+
+browser.close()
 
 result = list()
 counter = 0
@@ -50,18 +48,16 @@ async def get_pages(session, key, value):
     counter += 1
     print('getting link: ', counter)
 
-    link = f'https://www.youtube.com{key}'
-    async with session.get(link) as page:
+    async with session.get(key) as page:
         text = await page.read()
         sub_pattern = r'\d+\.?\d*.'
         pattern = f'"simpleText":"{sub_pattern} subscribers"'
         match = re.findall(pattern, text.decode('utf-8'))
         if match:
-            print(match[-1])
             subscribers = re.search(sub_pattern, match[-1]).group()
             result.append({
                 'name': value,
-                'link': link,
+                'link': key,
                 'subscribers': subscribers
             })
     print('finished link: ', local_counter + 1)
@@ -80,7 +76,7 @@ loop = asyncio.get_event_loop()
 loop.run_until_complete(async_requests(loop))
 
 with open('output.csv', 'w', encoding='utf-8') as file:
-    file.write('name,link,subscribers\n')
+    file.write('Name,Link,Subscribers count\n')
     for channel in result:
         string = f'{channel["name"]},{channel["link"]},{channel["subscribers"]}\n'
         file.write(string)
