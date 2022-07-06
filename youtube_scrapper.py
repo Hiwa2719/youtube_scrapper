3363dcdf934c3439d78a9e39dc38f6187d38f645
hiwa2719 <hiahmadyan@gmail.com>
2022-07-06 19:10:38 +0430

    first version of app

---
+++ youtube_scrapper.py
@@ -0,0 +1,84 @@
+import asyncio
+import re
+import time
+
+import aiohttp
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+message = """
+Please enter your search key word here
+(you can enter 'q' for exit)
+
+: """
+
+search_input = input(message)
+if search_input.lower() == 'q':
+    exit()
+
+search_input = search_input.replace(' ', '+')
+url = f'https://www.youtube.com/results?search_query={search_input}'
+
+browser = webdriver.Firefox()
+browser.get(url)
+
+search_items = {}
+
+while True:
+    time.sleep(2)
+    soup = BeautifulSoup(browser.page_source, 'lxml')
+    content = soup.find(id='contents')
+    items = content.find_all('ytd-video-renderer', {'class': 'style-scope ytd-item-section-renderer'})
+    if len(items) > 10:
+        for item in items:
+            a = item.find(id='text-container').find('a')
+            search_items.update({
+                a.get('href'): a.text
+            })
+        break
+    browser.execute_script("window.scrollBy(0, 15000)")
+
+result = list()
+counter = 0
+
+
+async def get_pages(session, key, value):
+    global counter
+    local_counter = counter
+    counter += 1
+    print('getting link: ', counter)
+
+    link = f'https://www.youtube.com{key}'
+    async with session.get(link) as page:
+        text = await page.read()
+        sub_pattern = r'\d+\.?\d*.'
+        pattern = f'"simpleText":"{sub_pattern} subscribers"'
+        match = re.findall(pattern, text.decode('utf-8'))
+        if match:
+            print(match[-1])
+            subscribers = re.search(sub_pattern, match[-1]).group()
+            result.append({
+                'name': value,
+                'link': link,
+                'subscribers': subscribers
+            })
+    print('finished link: ', local_counter + 1)
+
+
+async def async_requests(loop):
+    tasks = list()
+    async with aiohttp.ClientSession(loop=loop) as session:
+        for key, value in search_items.items():
+            tasks.append(get_pages(session, key, value))
+        grouped_tasks = asyncio.gather(*tasks)
+        return await grouped_tasks
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(async_requests(loop))
+
+with open('output.csv', 'w', encoding='utf-8') as file:
+    file.write('name,link,subscribers\n')
+    for channel in result:
+        string = f'{channel["name"]},{channel["link"]},{channel["subscribers"]}\n'
+        file.write(string)
